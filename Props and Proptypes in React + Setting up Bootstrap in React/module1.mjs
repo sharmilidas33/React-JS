2b9bd36c98320b19d785d54cc4159d735869747a
Sharmili Das <128738858+sharmilidas33@users.noreply.github.com>
2023-08-14 15:29:31 +0530

    Add files via upload

---
+++ Props and Proptypes in React + Setting up Bootstrap in React/module1.mjs
@@ -0,0 +1,6 @@
+const a="Priyanka";
+const b="Priyanshu";
+const c="Riya";
+export default a;
+export {b};
+export {c};
---
+++ Props and Proptypes in React + Setting up Bootstrap in React/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+function App() {
+  return (
+    //This is JSX.
+    <>
+    <Navbar title="TextUtils" aboutT="About Us"/>
+    </>
+  );
+}
+
+export default App;
